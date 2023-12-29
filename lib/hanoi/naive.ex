bf788dc7b76b6008f94b8a53be4e2d3cdc48b78f
Gareth Webber <github@garethwebber.com>
2023-12-29 17:03:11 +0000

    Renderer code centralised. Move stone function broken out.

--- lib/hanoi/naive.ex
+++ lib/hanoi/naive.ex
@@ -1,21 +1,27 @@
 defmodule Hanoi.Naive do
   require Logger
-  
+  require Hanoi.Render
+
   def move_stones(board) do
     disk_numbers = length(board.left)
     Logger.info "Processing #{disk_numbers} stones"
-    Logger.info "State: #{inspect board}"
+    Logger.info Hanoi.Render.render_to_string(board) 
     algo(board, disk_numbers, :left, :centre, :right)
   end
 
   defp algo(board, disk_numbers, source, auxilary, destination) do
    cond do 
-     disk_numbers == 1 -> Logger.info "#{source} -> #{destination}"
+     disk_numbers == 1 -> move_stone(board, source, destination) 
      true ->
        algo(board, disk_numbers - 1, source, destination, auxilary)
-       Logger.info "#{source} -> #{destination}"
+       move_stone(board, source, destination)
        algo(board, disk_numbers - 1, auxilary, source, destination)
     end
+  end
+  
+  defp move_stone(board, from, to) do
+    Logger.info "#{from} -> #{to}" 
+    Logger.info Hanoi.Render.render_to_string(board)
   end
 
 end
---
+++ lib/hanoi/render.ex
@@ -0,0 +1,6 @@
+defmodule Hanoi.Render do
+  
+  def render_to_string(board) do
+    "State: #{inspect board}" 
+  end
+end
