8845dde1ff63c7da2fa5d9eaa2980950f149172e
Gareth Webber <github@garethwebber.com>
2023-12-29 18:30:59 +0000

    Algorithm moves stones!

--- lib/hanoi/naive.ex
+++ lib/hanoi/naive.ex
@@ -4,24 +4,33 @@ defmodule Hanoi.Naive do
 
   def move_stones(board) do
     disk_numbers = length(board.left)
-    Logger.info "Processing #{disk_numbers} stones"
     Logger.info Hanoi.Render.render_to_string(board) 
     algo(board, disk_numbers, :left, :centre, :right)
   end
 
   defp algo(board, disk_numbers, source, auxilary, destination) do
    cond do 
-     disk_numbers == 1 -> move_stone(board, source, destination) 
+     disk_numbers == 1 -> 
+        move_stone(board, source, destination) 
      true ->
-       algo(board, disk_numbers - 1, source, destination, auxilary)
-       move_stone(board, source, destination)
-       algo(board, disk_numbers - 1, auxilary, source, destination)
+       intboard = algo(board, disk_numbers - 1, source, destination, auxilary)
+       newboard = move_stone(intboard, source, destination)
+       algo(newboard, disk_numbers - 1, auxilary, source, destination)
     end
   end
+
   
   defp move_stone(board, from, to) do
-    Logger.info "#{from} -> #{to}" 
-    Logger.info Hanoi.Render.render_to_string(board)
+    {:ok, [head|tail]} = Map.fetch(board, from) 
+    {:ok, to_contents} = Map.fetch(board, to)
+
+    Logger.info "Moving #{head} from  #{from} -> #{to}" 
+    
+    intboard = Map.put(board, from, tail)
+    newboard = Map.put(intboard, to, [head|to_contents])
+
+    Logger.info Hanoi.Render.render_to_string(newboard)
+    newboard
   end
 
 end
