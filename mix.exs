458b1fd19fbf3a60367cddb3c0e47f5ae1777da7
Gareth Webber <github@garethwebber.com>
2024-01-02 10:51:23 +0000

    Add documentation

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule TowerHanoi.MixProject do
       escript: escript_config(),
       version: "0.1.0",
       elixir: "~> 1.16",
+      name: "Towers of Hanoi",
+      source_url: "ihttps://github.com/garethwebber/elixir-tower-hanoi",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -23,6 +25,7 @@ defmodule TowerHanoi.MixProject do
   defp deps do
     [
       {:logger_file_backend, "~> 0.0.10"},
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false},
     ]
   end
   defp escript_config do
