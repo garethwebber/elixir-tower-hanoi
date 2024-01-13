a70b0f80da12d0a3586eb2e0cf56a0f8e1d98f2a
Gareth Webber <github@garethwebber.com>
2024-01-13 17:35:57 +0000

    Remove deuplication between similar functions

--- lib/hanoi/naive.ex
+++ lib/hanoi/naive.ex
@@ -11,24 +11,46 @@ defmodule Hanoi.Naive do
   """
   def get_moves(board) do
     disk_numbers = length(board.left)
-    {_board, moves} = moves_algo(board, [], disk_numbers, :left, :centre, :right)
+    {_board, moves} = moves_algo(board, [], disk_numbers, :left, :centre, :right, false)
     {:ok, Enum.reverse(moves)}
   end
+ 
+  @doc """
+  An algorithm to move stones between stacks by recursion 
+  """
+  def run_algo(board) do
+    disk_numbers = length(board.left)
+    IO.puts(Hanoi.Render.render_to_string(board))
+    {board, _moves} = moves_algo(board, [], disk_numbers, :left, :centre, :right, true)
+    board
+  end
 
-  defp moves_algo(board, moves, disk_numbers, source, auxilary, destination) do
-    cond do
-      disk_numbers == 1 ->
-        {:ok, newboard, newmoves} = record_stone_move(board, moves, source, destination)
-        {newboard, newmoves}
-
-      true ->
-        {intboard, intmoves} = moves_algo(board, moves, disk_numbers - 1, source, destination, auxilary)
-        {:ok, newboard, newmoves} = record_stone_move(intboard, intmoves, source, destination)
-        moves_algo(newboard, newmoves, disk_numbers - 1, auxilary, source, destination)
+  @doc """
+  Returns a new board with stone moved, or an error if an invalid move 
+  """
+  def move_stone(board, from, to, debug) do
+    with {:ok, newboard, _moves} <- record_stone_move(board, [], from, to, debug)
+    do 
+      {:ok, newboard}
+    else _ -> 
+      {:error, board}
     end
   end
 
-  defp record_stone_move(board, moves, from, to) do
+  defp moves_algo(board, moves, disk_numbers, source, auxilary, destination, debug) do
+    cond do
+      disk_numbers == 1 ->
+        {:ok, newboard, newmoves} = record_stone_move(board, moves, source, destination, debug)
+        {newboard, newmoves}
+
+      true ->
+        {intboard, intmoves} = moves_algo(board, moves, disk_numbers - 1, source, destination, auxilary, debug)
+        {:ok, newboard, newmoves} = record_stone_move(intboard, intmoves, source, destination, debug)
+        moves_algo(newboard, newmoves, disk_numbers - 1, auxilary, source, destination, debug)
+    end
+  end
+
+  defp record_stone_move(board, moves, from, to, debug) do
     case is_valid_move(board, from, to) do
       false ->
         Logger.error("Invalid move #{from}->#{to}")
@@ -41,52 +63,10 @@ defmodule Hanoi.Naive do
         intboard = Map.put(board, from, tail)
         newboard = Map.put(intboard, to, [head | to_contents])
         
-        {:ok, newboard, [{from, to} | moves]}
-    end
-  end
-
-  @doc """
-  An algorithm to move stones between stacks by recursion 
-  """
-  def run_algo(board) do
-    disk_numbers = length(board.left)
-    IO.puts(Hanoi.Render.render_to_string(board))
-    algo(board, disk_numbers, :left, :centre, :right)
-  end
-
-  defp algo(board, disk_numbers, source, auxilary, destination) do
-    cond do
-      disk_numbers == 1 ->
-        {:ok, newboard} = move_stone(board, source, destination, true)
-        newboard
-
-      true ->
-        intboard = algo(board, disk_numbers - 1, source, destination, auxilary)
-        {:ok, newboard} = move_stone(intboard, source, destination, true)
-        algo(newboard, disk_numbers - 1, auxilary, source, destination)
-    end
-  end
-
-  @doc """
-  Returns a new board with stone moved, or an error if an invalid move 
-  """
-  def move_stone(board, from, to, debug) do
-    case is_valid_move(board, from, to) do
-      false ->
-        Logger.error("Invalid move #{from}->#{to}")
-        {:error, board}
-
-      true ->
-        {:ok, [head | tail]} = Map.fetch(board, from)
-        {:ok, to_contents} = Map.fetch(board, to)
-
-        intboard = Map.put(board, from, tail)
-        newboard = Map.put(intboard, to, [head | to_contents])
-
         if debug do
           IO.puts(Hanoi.Render.render_to_string(newboard))
-        end
-        {:ok, newboard}
+        end 
+        {:ok, newboard, [{from, to} | moves]}
     end
   end
 
