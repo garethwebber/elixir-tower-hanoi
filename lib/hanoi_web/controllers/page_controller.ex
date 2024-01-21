9a9af82c12af71ce20e95fabdb5cdeb10558b07d
Gareth Webber <github@garethwebber.com>
2024-01-21 15:36:25 +0000

    All buttons

--- lib/hanoi_web/controllers/page_controller.ex
+++ lib/hanoi_web/controllers/page_controller.ex
@@ -9,13 +9,27 @@ defmodule HanoiWeb.ControllerLive do
 
   def render(assigns) do
     Logger.info("Render")
-    ~H"""
+    ~L"""
        <div class="lg:block">
         <pre>
           <%= Hanoi.Render.render_to_string(@state)  %>
         </pre>
+        <table border="10">
+        <tr><td>
         <button phx-click="move_stone" phx-value-from="left" phx-value-to="centre">Left to centre</button>
+        </td><td>
+        <button phx-click="move_stone" phx-value-from="centre" phx-value-to="left">Centre to left</button>
+        </td><td>
+        <button phx-click="move_stone" phx-value-from="centre" phx-value-to="right">Centre to right</button>
+        </td><td>
+        <button phx-click="move_stone" phx-value-from="right" phx-value-to="centre">Right to centre</button>
+        </td><tr>
+        <tr><td>
         <button phx-click="move_stone" phx-value-from="left" phx-value-to="right">Left to right</button>
+        </td><td></td><td></td><td>
+        <button phx-click="move_stone" phx-value-from="right" phx-value-to="left">Right to Left</button>
+        </td></tr>
+        </table>
       </div>
     """
   end
