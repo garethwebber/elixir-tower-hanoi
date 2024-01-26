f7cc5e7d80cd79eefc543ffa08944f8edaed6051
Gareth Webber <github@garethwebber.com>
2024-01-26 17:20:02 +0800

    Documentation

--- lib/hanoi_server/tower_game.ex
+++ lib/hanoi_server/tower_game.ex
@@ -1,6 +1,12 @@
 defmodule Hanoi.TowerGame do
   use Supervisor
 
+  @moduledoc """
+  The main entry point for the Hanoi game. Runs as a supervisor and starts
+  a GenServer to process work and an ETS instance to hold state. 
+  """
+
+  @doc false
   def child_spec(opts) do
     %{
       id: (opts[:name] || raise ArgumentError, "Cache name is required"),
@@ -8,36 +14,62 @@ defmodule Hanoi.TowerGame do
     }
   end
 
+  @doc """
+  Call this function to start up the module. Name and Stones must be provided. 
+  """
   def start_link(opts) do
     name = opts[:name] || raise ArgumentError, "Cache name is required"
     stones = opts[:stones] || raise ArgumentError, "Number of stones required"
     Supervisor.start_link(__MODULE__, opts, [name: name, stones: stones])
   end
-
+  
+  @doc """
+  Function returning the current state of the hanoi board
+  """
   def get_state(name) do
     Hanoi.TowerState.get_state(storage_name(name))
   end
 
+  @doc """
+  Debug function returning current state of hanoi board in a textual
+  representation.
+  """
   def get_state_as_text(name) do
     Hanoi.TowerState.get_state_as_text(storage_name(name))
   end
 
+  @doc """
+  Function returning the number of moves that have been played.
+  """
   def get_number_moves(name) do
     Hanoi.TowerState.get_number_moves(storage_name(name))
   end
 
+  @doc """
+  Function that returns the number of stones on the board.
+  """
   def get_number_stones(name) do
      Hanoi.TowerState.get_number_stones(storage_name(name))
   end
   
+  @doc """
+  Funtion that moves a stone between piles. 
+  Will error if the stone is bigger than the one at the top of the pile.
+  Increments moves taken.
+  """
   def move_stone(name, from, to) do
     Hanoi.TowerState.move_stone(storage_name(name), from, to)
   end
 
+  @doc """
+  Function that for a board with all stones on left pile will move them
+  in a legal way (no big stone on small stone) to the right hand pile
+  """
   def get_moves(name) do
     Hanoi.TowerState.get_moves(storage_name(name))
   end
 
+  @doc false
   def init(opts) do
     name = opts[:name]
     stones = opts[:stones]
