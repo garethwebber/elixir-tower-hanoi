4d3f776c21c4e4613cb004ca28073f1a1d7caf18
Gareth Webber <github@garethwebber.com>
2024-02-05 11:00:19 +0000

    Formatter

--- lib/hanoi_web/controllers/hanoi_game_controller_live.ex
+++ lib/hanoi_web/controllers/hanoi_game_controller_live.ex
@@ -2,6 +2,7 @@ defmodule HanoiWeb.HanoiGameControllerLive do
   use HanoiWeb, :html
   use Phoenix.LiveView
   import HanoiWeb.HanoiGameView
+
   @moduledoc """
   Controller that renders the main Hanoi game page
 
@@ -13,16 +14,18 @@ defmodule HanoiWeb.HanoiGameControllerLive do
 
   @doc "Set up initial liveview state"
   def mount(_params, _, socket) do
-    state = Hanoi.TowerGame.get_state(:hanoi) 
+    state = Hanoi.TowerGame.get_state(:hanoi)
     number_moves = Hanoi.TowerGame.get_number_moves(:hanoi)
     number_stones = Hanoi.TowerGame.get_number_stones(:hanoi)
-    {:ok, assign(socket, 
-        state: state,
-        number_moves: number_moves,
-        number_stones: number_stones,
-        error_text: nil,
-        auto_mode: false
-        )}
+
+    {:ok,
+     assign(socket,
+       state: state,
+       number_moves: number_moves,
+       number_stones: number_stones,
+       error_text: nil,
+       auto_mode: false
+     )}
   end
 
   @doc "Render the page based on current state"
@@ -56,70 +59,71 @@ defmodule HanoiWeb.HanoiGameControllerLive do
         </main>
     """
   end
-  
+
   @doc "Handle the auto_mode, move_stone and reset clicks from the page"
   def handle_event("move_stone", %{"from" => from, "to" => to}, socket) do
-    value = Hanoi.TowerGame.move_stone(:hanoi, 
-                                        stone_name(from), stone_name(to))
-    error_text= case value do
-      :ok    -> nil
-      :error -> "Invalid move #{from} -> #{to}"
-    end
+    value = Hanoi.TowerGame.move_stone(:hanoi, stone_name(from), stone_name(to))
 
-    {:noreply, assign(socket, 
-        state: Hanoi.TowerGame.get_state(:hanoi),
-        number_moves: Hanoi.TowerGame.get_number_moves(:hanoi),
-        error_text: error_text 
-        )}
+    error_text =
+      case value do
+        :ok -> nil
+        :error -> "Invalid move #{from} -> #{to}"
+      end
+
+    {:noreply,
+     assign(socket,
+       state: Hanoi.TowerGame.get_state(:hanoi),
+       number_moves: Hanoi.TowerGame.get_number_moves(:hanoi),
+       error_text: error_text
+     )}
   end
 
   # Creates a new board with X stones, and resets error_text and move_counter
   def handle_event("reset", %{"stone" => stone}, socket) do
+    {new_stones, _rest} = Integer.parse(stone)
+    Hanoi.TowerGame.reset(:hanoi, new_stones)
 
-      {new_stones, _rest} = Integer.parse(stone) 
-      Hanoi.TowerGame.reset(:hanoi, new_stones)
-
-      {:noreply, assign(socket,
-        state: Hanoi.TowerGame.get_state(:hanoi),
-        number_moves: Hanoi.TowerGame.get_number_moves(:hanoi),
-        number_stones: new_stones,
-        error_text: nil, 
-        auto_mode: false
-        )}         
+    {:noreply,
+     assign(socket,
+       state: Hanoi.TowerGame.get_state(:hanoi),
+       number_moves: Hanoi.TowerGame.get_number_moves(:hanoi),
+       number_stones: new_stones,
+       error_text: nil,
+       auto_mode: false
+     )}
   end
- 
+
   # Demo auto-mode: disables manual controls then moves stones automatically
   def handle_event("auto_mode", _params, socket) do
     live_view_pid = self()
 
-      socket
-      |> start_async(:running_task, fn ->
-          
-          moves = Hanoi.TowerGame.get_moves(:hanoi)
+    socket
+    |> start_async(:running_task, fn ->
+      moves = Hanoi.TowerGame.get_moves(:hanoi)
 
-          Enum.map(moves, fn({from, to}) ->
-            :timer.sleep(250)
-            Hanoi.TowerGame.move_stone(:hanoi, from, to)
-            send(live_view_pid, :refresh_board)
-          end)
-    
-        end)
+      Enum.map(moves, fn {from, to} ->
+        :timer.sleep(250)
+        Hanoi.TowerGame.move_stone(:hanoi, from, to)
+        send(live_view_pid, :refresh_board)
+      end)
+    end)
 
-     {:noreply, assign(socket, 
-        error_text: "Running auto-mode",
-        auto_mode: true
+    {:noreply,
+     assign(socket,
+       error_text: "Running auto-mode",
+       auto_mode: true
      )}
   end
 
   @doc "Handle the refresh_board messages triggered by auto_mode"
   def handle_info(:refresh_board, socket) do
-    {:noreply, assign(socket, 
-        state: Hanoi.TowerGame.get_state(:hanoi)
-        )}
+    {:noreply,
+     assign(socket,
+       state: Hanoi.TowerGame.get_state(:hanoi)
+     )}
   end
-  
+
   defp stone_name(name) do
     :"#{name}"
   end
-
 end
--- lib/hanoi_web/controllers/hanoi_game_view.ex
+++ lib/hanoi_web/controllers/hanoi_game_view.ex
@@ -1,46 +1,46 @@
 defmodule HanoiWeb.HanoiGameView do
-   use HanoiWeb, :html
-   use Phoenix.LiveView
-   @moduledoc """
-   Supporing functions for the main page rendering controller
-   """
+  use HanoiWeb, :html
+  use Phoenix.LiveView
 
-   @doc "Define the CSS needed to render the stones board"
-   def render_css(assigns) do
+  @moduledoc """
+  Supporing functions for the main page rendering controller
+  """
 
-     ~H"""
-       <style>
-       .stone {
-          margin:            0 auto;
-          height:            20px;
-          background-color:  --var(color2)
-          justify-content:   center;
-       }
-       .pile {
-          display:           inline;
-          width:             33%;
-          margin-top:        auto;
-       }     
-       .board {
-          display:          flex;
-          flex-direction:   row;
-          width:            100%;
-          height:           <%= (@number_stones * 20) + 40 %>px;
-          border-bottom:    solid;
-          padding:          0;
-       }
-        :root {
-           --color1: #155263; 
-           --color2: #ff6f3c; 
-           --color3: #ff9a3c; 
-           --color4: #ffc93c; 
-        }
-       </style>
-      """
-   end
+  @doc "Define the CSS needed to render the stones board"
+  def render_css(assigns) do
+    ~H"""
+     <style>
+     .stone {
+        margin:            0 auto;
+        height:            20px;
+        background-color:  --var(color2)
+        justify-content:   center;
+     }
+     .pile {
+        display:           inline;
+        width:             33%;
+        margin-top:        auto;
+     }     
+     .board {
+        display:          flex;
+        flex-direction:   row;
+        width:            100%;
+        height:           <%= (@number_stones * 20) + 40 %>px;
+        border-bottom:    solid;
+        padding:          0;
+     }
+      :root {
+         --color1: #155263; 
+         --color2: #ff6f3c; 
+         --color3: #ff9a3c; 
+         --color4: #ffc93c; 
+      }
+     </style>
+    """
+  end
 
-   @doc "Renders the board as a pile of stones" 
-   def render_board(assigns) do
+  @doc "Renders the board as a pile of stones"
+  def render_board(assigns) do
     ~H"""
       <!--<%= width = div(100, @number_stones) %>-->
       <div name="board" class="board">
@@ -59,26 +59,26 @@ defmodule HanoiWeb.HanoiGameView do
 
       </div>
     """
-   end
+  end
 
-   @doc "Render each individual pile on the board"
-   def render_pile([], _width) do
-     "\n"
-   end
+  @doc "Render each individual pile on the board"
+  def render_pile([], _width) do
+    "\n"
+  end
 
-   def render_pile(pile, width) do
-     [head|tail] = pile
-     render_stone(head, width) <> render_pile(tail, width) 
-   end
-   
-   @doc "Render an individual stone in a pile"
-   def render_stone(stone, width) do
-      color = Integer.mod(stone, 4)
-      stone_width = stone*width
-      "<div name=\"st#{stone}\" class=\"stone\" 
+  def render_pile(pile, width) do
+    [head | tail] = pile
+    render_stone(head, width) <> render_pile(tail, width)
+  end
+
+  @doc "Render an individual stone in a pile"
+  def render_stone(stone, width) do
+    color = Integer.mod(stone, 4)
+    stone_width = stone * width
+    "<div name=\"st#{stone}\" class=\"stone\" 
               style=\"width:#{stone_width}%; background: var(--color#{color + 1})\"></div>"
-   end
-  
+  end
+
   @doc "Renders reset dropdown and button"
   def render_reset_block(assigns) do
     form = to_form(%{"stone" => 3})
@@ -100,20 +100,21 @@ defmodule HanoiWeb.HanoiGameView do
   @doc "Renders any error messages"
   def render_error_text(%{error_text: error_text} = assigns) do
     cond do
-      error_text == nil -> ~H"""
-                              <p>&nbsp</p><br/>
-                            """
+      error_text == nil ->
+        ~H"""
+          <p>&nbsp</p><br/>
+        """
 
-      true  ->
-            ~H"""
-               <p align="center"><span style="color: #c51244; border: 1px solid #c51244 !important;">
-                 &nbsp; 
-                 <.icon name="hero-exclamation-circle-mini" class="py-2.5 h-4 w-4 text-rose-500"/>
-                 <%= @error_text  %>
-                 &nbsp;
-               </span></p><br />
-             """
-      end
+      true ->
+        ~H"""
+          <p align="center"><span style="color: #c51244; border: 1px solid #c51244 !important;">
+            &nbsp; 
+            <.icon name="hero-exclamation-circle-mini" class="py-2.5 h-4 w-4 text-rose-500"/>
+            <%= @error_text  %>
+            &nbsp;
+          </span></p><br />
+        """
+    end
   end
 
   @doc "Renders the current move count"
@@ -125,26 +126,26 @@ defmodule HanoiWeb.HanoiGameView do
 
   @doc "Renders the controls for moving stones"
   def render_game_controls(assigns) do
-      ~H"""
-        <table border="10">
-        <tr><td>
-        <.button phx-click="move_stone" phx-value-from="left" phx-value-to="centre">Left to centre</.button>
-        </td><td>
-        <.button phx-click="move_stone" phx-value-from="centre" phx-value-to="left">Centre to left</.button>
-        </td><td>
-        <.button phx-click="move_stone" phx-value-from="centre" phx-value-to="right">Centre to right</.button>
-        </td><td>
-        <.button phx-click="move_stone" phx-value-from="right" phx-value-to="centre">Right to centre</.button>
-        </td></tr>
-        <tr><td>
-        <.button phx-click="move_stone" phx-value-from="left" phx-value-to="right">Left to right</.button>
-        </td>
-        <td></td><td></td>
-        <td>
-        <.button phx-click="move_stone" phx-value-from="right" phx-value-to="left">Right to Left</.button>
-        </td></tr>
-        </table>
-     """
+    ~H"""
+       <table border="10">
+       <tr><td>
+       <.button phx-click="move_stone" phx-value-from="left" phx-value-to="centre">Left to centre</.button>
+       </td><td>
+       <.button phx-click="move_stone" phx-value-from="centre" phx-value-to="left">Centre to left</.button>
+       </td><td>
+       <.button phx-click="move_stone" phx-value-from="centre" phx-value-to="right">Centre to right</.button>
+       </td><td>
+       <.button phx-click="move_stone" phx-value-from="right" phx-value-to="centre">Right to centre</.button>
+       </td></tr>
+       <tr><td>
+       <.button phx-click="move_stone" phx-value-from="left" phx-value-to="right">Left to right</.button>
+       </td>
+       <td></td><td></td>
+       <td>
+       <.button phx-click="move_stone" phx-value-from="right" phx-value-to="left">Right to Left</.button>
+       </td></tr>
+       </table>
+    """
   end
 
   @doc "Renders the demo automode block"
@@ -154,5 +155,4 @@ defmodule HanoiWeb.HanoiGameView do
       <.button class="bg-rose-700 hover:bg-rose-500" phx-click="auto_mode">Auto mode</.button>
     """
   end
-
 end
