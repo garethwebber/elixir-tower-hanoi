6d990936ecb90d125664a63408a30ac657b77314
Gareth Webber <github@garethwebber.com>
2024-02-20 21:12:33 +0000

    Add typespecs to public API

--- lib/hanoi_server/tower_game.ex
+++ lib/hanoi_server/tower_game.ex
@@ -22,6 +22,7 @@ defmodule Hanoi.TowerGame do
   @doc """
   Call this function to start up the module. Name and Stones must be provided. 
   """
+  @spec start_link(opts :: list()) :: {:error, any()} | {:ok, pid()} 
   def start_link(opts) do
     _name = opts[:name] || raise ArgumentError, "Cache name is required"
     _stones = opts[:stones] || raise ArgumentError, "Number of stones required"
@@ -31,6 +32,7 @@ defmodule Hanoi.TowerGame do
   @doc """
   Function returning the current state of the hanoi board
   """
+  @spec get_state(name :: atom()) :: Hanoi.Board.t()
   def get_state(name) do
     Hanoi.TowerState.get_state(storage_name(name))
   end
@@ -39,6 +41,7 @@ defmodule Hanoi.TowerGame do
   Debug function returning current state of hanoi board in a textual
   representation.
   """
+  @spec get_state_as_text(name :: atom()) :: String.t()
   def get_state_as_text(name) do
     Hanoi.TowerState.get_state_as_text(storage_name(name))
   end
@@ -46,6 +49,7 @@ defmodule Hanoi.TowerGame do
   @doc """
   Function returning the number of moves that have been played.
   """
+  @spec get_number_moves(name :: atom()) :: non_neg_integer()
   def get_number_moves(name) do
     Hanoi.TowerState.get_number_moves(storage_name(name))
   end
@@ -53,6 +57,7 @@ defmodule Hanoi.TowerGame do
   @doc """
   Function that returns the number of stones on the board.
   """
+  @spec get_number_stones(name :: atom()) :: non_neg_integer()
   def get_number_stones(name) do
     Hanoi.TowerState.get_number_stones(storage_name(name))
   end
@@ -62,6 +67,7 @@ defmodule Hanoi.TowerGame do
   Will error if the stone is bigger than the one at the top of the pile.
   Increments moves taken.
   """
+  @spec move_stone(name :: atom(), from :: atom(), to :: atom()) :: :ok | :error
   def move_stone(name, from, to) do
     Hanoi.TowerState.move_stone(storage_name(name), from, to)
   end
@@ -70,6 +76,7 @@ defmodule Hanoi.TowerGame do
   Funtion returns true if game is complete
   That is all stones in correct order on right hand pile
   """
+  @spec is_complete(name :: atom()) :: boolean() | :error
   def is_complete(name) do
     Hanoi.TowerState.is_complete(storage_name(name))
   end
@@ -78,10 +85,12 @@ defmodule Hanoi.TowerGame do
   Function that for a board with all stones on left pile will move them
   in a legal way (no big stone on small stone) to the right hand pile
   """
+  @spec get_moves(name :: atom()) :: list() | :error
   def get_moves(name) do
     Hanoi.TowerState.get_moves(storage_name(name))
   end
 
+  @spec reset(name :: atom(), new_stones :: pos_integer()) :: :ok
   def reset(name, new_stones) do
     Hanoi.TowerState.reset(storage_name(name), new_stones)
   end
