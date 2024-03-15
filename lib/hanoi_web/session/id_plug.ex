892608ce973405d648d3c386f300d9d01bdd085a
Gareth Webber <me@Gareths-Mac-Studio.local>
2024-03-15 10:26:57 +0000

    Document session modules

--- lib/hanoi_web/session/id_plug.ex
+++ lib/hanoi_web/session/id_plug.ex
@@ -1,10 +1,20 @@
 defmodule HanoiWeb.Session.IdPlug do
   @behaviour Plug
   import Plug.Conn
+  @moduledoc """
+  This module plugs into the phoenix request handling and checks is a session_id exists. If it doesn't
+  it creates one based on a 4-digit hexadecimal random number.
 
+  A TowerState GenServer and ETS table are created per-session and use this id as their unique name.
+  """
+
+  @doc false
   @impl true
   def init(default), do: default
 
+  @doc """
+  Check session for an :session_id variable and create one if missing.
+  """
   @impl true
   def call(conn, _config) do
     case get_session(conn, :session_id) do
--- lib/hanoi_web/session/purger.ex
+++ lib/hanoi_web/session/purger.ex
@@ -1,19 +1,35 @@
 defmodule HanoiWeb.Session.Purge do
   use GenServer
   require Logger
+  @moduledoc """
+  The game creates a TowerState GenServer process and ETS table per user-session.
+
+  To make sure these don't build up over time, we check for last used time (created, or last move). Every
+  purge_gap, a process runs looking for old sessions - that is last used > purge_time - and deletes them.
+  """
+alias Code.Identifier
 
   @purge_gap 600000
 
+  @doc false
   def start_link(opts) do
     GenServer.start_link(__MODULE__, opts)
   end
 
+  @doc """
+  Stores :purge_time in GenServer state
+  Sends first message to :run_purge, starting the continuous purging cycle.
+  """
   def init(opts) do
     purge_time = opts[:purge_time]
     Process.send_after(self(), :run_purge, @purge_gap)
     {:ok, purge_time}
   end
 
+  @doc """
+  Function collects all the current game sessions, filters by age and deletes old ones.
+  It then calls itself creating continuous loop of purging.
+  """
   def handle_info(:run_purge, purge_time) do
     Hanoi.TowerGame.show_games()
     |> Enum.filter(&filter_age(&1, purge_time))
@@ -37,6 +53,6 @@ defmodule HanoiWeb.Session.Purge do
     name = game_info[:name]
     pid = game_info[:pid]
     Logger.info("Purging session #{name}")
-    games = Hanoi.TowerGame.delete_game(pid)
+    Hanoi.TowerGame.delete_game(pid)
   end
 end
